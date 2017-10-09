eb372c18c58367edaf355d40419dceb769e2e07f
Thomas Duerr <thomas.duerr@arvato.com>
2017-10-09 20:21:03 +0200

    homwbrew

--- homebrew.sh
+++ homebrew.sh
@@ -6,7 +6,14 @@ brew up
 brew tap homebrew/aliases
 brew install tmux
 brew install tree jq hr fzf ack wget htop
-brew install caskroom/cask/java
+brew install git git-flow bash-completion
+
+# to see which versions are currently installed:
+#   /usr/libexec/java_home -V
+#
+# see also: http://mattshomepage.com/articles/2016/May/22/java_home_mac_os_x/
+brew cask install java
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 
 # docker
 # se  https://pilsniak.com/how-to-install-docker-on-mac-os-using-brew/
@@ -18,3 +25,8 @@ sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machi
 # apps
 brew install caskroom/cask/brew-cask
 brew cask install visual-studio-code
+
+
+# update personal addresses in
+#   .npmrc
+#   .gitconfig
