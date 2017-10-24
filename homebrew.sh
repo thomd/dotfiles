50ece11c0e7c4d1636fdd0c81ddd7b7f4e6bfce1
Thomas Dürr <thomduerr@gmail.com>
2017-10-24 16:05:47 +0200

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -15,25 +15,32 @@ export NVM_DIR=$HOME/.nvm
 . /usr/local/opt/nvm/nvm.sh
 nvm install --lts
 
-# to see which versions are currently installed:
-#   /usr/libexec/java_home -V
-#
-# see also: http://mattshomepage.com/articles/2016/May/22/java_home_mac_os_x/
-brew cask install java
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
-
-# docker
-# se  https://pilsniak.com/how-to-install-docker-on-mac-os-using-brew/
-brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
-sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-
-
 # apps
 brew tap caskroom/cask
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install visual-studio-code
+
+# java
+#
+#   to see which versions are currently installed (http://mattshomepage.com/articles/2016/May/22/java_home_mac_os_x):
+#     /usr/libexec/java_home -V
+#
+brew cask install java
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+
+# docker
+#
+echo "install docker from https://docs.docker.com/docker-for-mac/install"
+#
+#   see https://pilsniak.com/how-to-install-docker-on-mac-os-using-brew/
+brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
+sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+
+
+
+
 
 
 # update personal addresses in
