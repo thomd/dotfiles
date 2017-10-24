8ade293cd7e606ab4383d2a3293bb64770fc8586
Thomas Dürr <thomduerr@gmail.com>
2017-10-24 14:09:39 +0200

    Create macbook.sh

---
+++ macbook.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Xcode
+#
+# Install Xcode from the App store or the Apple developer website
+#
+# For installing Xcode command line tools run the command
+xcode-select --install
+
+# Homebrew
+source ./homebrew.sh
+
