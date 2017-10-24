23dfe8fa82c57379074e3bb35606ae77381c5497
Thomas Dürr <thomduerr@gmail.com>
2017-10-24 17:44:18 +0200

    Update macbook.sh

--- macbook.sh
+++ macbook.sh
@@ -10,3 +10,17 @@ xcode-select --install
 # Homebrew
 source ./homebrew.sh
 
+
+
+# github
+# 
+# generate new SSH key (https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/) or use existing
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/github
+
+
+# company settings
+npm config set proxy http://proxy3:8080
+npm config set strict-ssl false
+export http_proxy=http://proxy3:8080
+export https_proxy=http://proxy3:8080
